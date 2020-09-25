0633d10e5ab405b06cc87d5cbed5d47ca9d2740b
blindpentester <71842000+blindpentester@users.noreply.github.com>
2020-09-24 18:19:58 -0600

    Update the_essentials.sh

--- the_essentials.sh
+++ the_essentials.sh
@@ -14,11 +14,6 @@ fix_sources() {
 	}
 
 
-install_go() {
-	sudo apt install golang -y
-	}
-
-
 install_terminator() {
 	sudo apt install terminator -y
 	}
@@ -355,7 +350,6 @@ install_stegoVeritas() {
 	
 
 fix_sources
-install_go
 pimpmykali
 install_ffuf
 install_p0wny_shell
